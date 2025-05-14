628e83834a376ba0b1ed63977ae1987bdee6b9fa
Daniel Tisza <danieltisza23@gmail.com>
2025-05-14 23:57:11 +0200

    added basic routinh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MatchesComponent } from './pages/matches/matches.component';
+import { NewsComponent } from "./pages/news/news.component";
+import { PlayersComponent } from './pages/players/players.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'matches', component: MatchesComponent },
+    { path: 'news', component: NewsComponent },
+    { path: 'players', component: PlayersComponent },
+    { path: '**', redirectTo: '' }
+];
