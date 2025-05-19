3a70e6ed56dd4886cf3c5145d8cec3af841877a4
Daniel Tisza <danieltisza23@gmail.com>
2025-05-19 23:33:11 +0200

    added some navbar styling - need to improve

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MatchesComponent } from './pages/matches/matches.component';
 import { NewsComponent } from "./pages/news/news.component";
 import { PlayersComponent } from './pages/players/players.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', component: HomeComponent, pathMatch:'full'},
     { path: 'matches', component: MatchesComponent },
     { path: 'news', component: NewsComponent },
     { path: 'players', component: PlayersComponent },
